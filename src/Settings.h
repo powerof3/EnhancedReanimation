7586eefd93164d3403830469440c778328e8a796
powerof3 <32599957+powerof3@users.noreply.github.com>
2021-11-27 16:17:04 +0530

    converted project to cmake

---
+++ src/Settings.h
@@ -0,0 +1,45 @@
+#pragma once
+
+class Settings
+{
+public:
+	[[nodiscard]] static Settings* GetSingleton()
+	{
+		static Settings singleton;
+		return std::addressof(singleton);
+	}
+
+	void Load()
+	{				
+		constexpr auto path = L"Data/SKSE/Plugins/po3_EnhancedReanimation.ini";
+
+		CSimpleIniA ini;
+		ini.SetUnicode();
+
+		ini.LoadFile(path);
+
+		detail::get_value(ini, fastTravel, "Settings", "Fast Travel", ";fast travel fix");
+		detail::get_value(ini, npcCombat, "Settings", "Necromancer Cast", ";reanimated necromancers can cast reanimate spells");
+		detail::get_value(ini, decapitateCheck, "Settings", "Decapitated NPCs", ";reanimate decapitated enemies");
+		detail::get_value(ini, rideHorse, "Riding", "Ride Horses", ";ride reanimated horses/mounts (not dragons)");
+		detail::get_value(ini, decapitateCheck, "Riding", "Patch Horses", ";patch all reanimate spells so mounts can be reanimated");
+
+		ini.SaveFile(path);
+	}
+
+	bool fastTravel{ true };
+	bool npcCombat{ true };
+	bool decapitateCheck{ true };
+	bool rideHorse{ true };
+	bool patchHorse{ true };
+
+private:
+	struct detail
+	{
+		static void get_value(CSimpleIniA& a_ini, bool& a_value, const char* a_section, const char* a_key, const char* a_comment)
+		{
+			a_value = a_ini.GetBoolValue(a_section, a_key, a_value);
+			a_ini.SetBoolValue(a_section, a_key, a_value, a_comment);
+		};
+	};
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,292 +1,252 @@
-﻿#include "version.h"
+#include "Settings.h"
 
-//reanimate state = (actorState1_C0 & 0x1E00000) != 0x800000
-
-static bool fastTravel = true;
-static bool npcCombat = true;
-static bool decapitateCheck = true;
-static bool rideHorse = true;
-static bool patchHorse = true;
-
-
-class NPCCombatCast	 //nop out reanimated lifestate check
+namespace NPCCombatCast
 {
-public:
-	static void Patch()
+	void Patch()
 	{
-		REL::Relocation<std::uintptr_t> CombatMagicCastReanimate{ REL::ID(44036) };
+		//CombatMagicCasterReanimate::CheckShouldEquip
+		REL::Relocation<std::uintptr_t> target{ REL::ID(44036) };
 
-		constexpr std::array<std::uint8_t, 2> bytes{
-			0x90,
-			0x90
-		};
-		REL::safe_write(CombatMagicCastReanimate.address() + 0xAF, stl ::span{ bytes.data(), bytes.size() });
+		constexpr std::array<std::uint8_t, 2> bytes{ REL::NOP, REL::NOP };
+		REL::safe_write(target.address() + 0xAF, std::span{ bytes.data(), bytes.size() });
 	}
 };
 
-
-class ReanimateCheck  //using IsLimbGone (aka decapitate)
+namespace DecapitateCheck
 {
-public:
-	static void Patch()
+	struct IsDismembered
 	{
-		REL::Relocation<std::uintptr_t> CanActorBeReanimated{ REL::ID(33825) };
+		static bool thunk(RE::Actor*, std::uint32_t)
+		{
+			return false;
+		}
+		static inline REL::Relocation<decltype(&thunk)> func;
+	};
 
-		auto& trampoline = SKSE::GetTrampoline();
-		_IsLimbGone = trampoline.write_call<5>(CanActorBeReanimated.address() + 0x72, IsLimbGone);
-	}
-
-private:
-	static bool IsLimbGone(RE::Actor* a_actor, std::uint32_t a_limbEnum)
+	void Patch()
 	{
-		return false;
+		//Actor::CanBeReanimated
+		REL::Relocation<std::uintptr_t> target{ REL::ID(33825) };
+		stl::write_thunk_call<IsDismembered>(target.address() + 0x72);
 	}
-	static inline REL::Relocation<decltype(IsLimbGone)> _IsLimbGone;
-};
+}
 
-
-class FastTravel  //moveto right after the game does it, but before the event sink is dispatched
+namespace FastTravel
 {
-public:
-	static void Patch()
+	struct ClearPlayerCombatGroup
 	{
-		REL::Relocation<std::uintptr_t> HandleFastTravel{ REL::ID(39373) };
+		static void thunk(RE::PlayerCharacter* a_player)
+		{
+			func(a_player);
 
-		auto& trampoline = SKSE::GetTrampoline();
-		_ClearPlayerCombatGroup = trampoline.write_call<5>(HandleFastTravel.address() + 0xA4C, ClearPlayerCombatGroup);	 //arbitary function, called after package processing
-	}
-
-private:
-	static void ClearPlayerCombatGroup(RE::PlayerCharacter* a_player)
-	{
-		_ClearPlayerCombatGroup(a_player);
-
-		auto extraFollowers = a_player->extraList.GetByType<RE::ExtraFollower>();
-		if (extraFollowers) {
-			for (auto& followers : extraFollowers->actorFollowers) {
-				auto actorPtr = followers.actor.get();
-				auto actor = actorPtr.get();
-				if (actor && actor->GetLifeState() == RE::ACTOR_LIFE_STATE::kReanimate && !actor->IsAMount()) {
-					auto distance = a_player->GetPosition().GetSquaredDistance(actor->GetPosition());
-					if (distance > followDistSquared) {
-						actor->MoveTo(a_player);
+			const auto extraFollowers = a_player->extraList.GetByType<RE::ExtraFollower>();
+			if (extraFollowers) {
+				for (const auto& followers : extraFollowers->actorFollowers) {
+					auto actor = followers.actor.get();
+					if (actor && actor->GetLifeState() == RE::ACTOR_LIFE_STATE::kReanimate && !actor->IsAMount()) {
+						const auto distance = a_player->GetPosition().GetSquaredDistance(actor->GetPosition());
+						if (distance > followDistSquared) {
+							actor->MoveTo(a_player);
+						}
 					}
 				}
 			}
 		}
-	}
-	static inline REL::Relocation<decltype(ClearPlayerCombatGroup)> _ClearPlayerCombatGroup;
+		static inline REL::Relocation<decltype(&thunk)> func;
 
-	static inline float followDistSquared = 160000.0f;
-};
+		static inline float followDistSquared = 160000.0f;
+	};
 
-
-class RideHorse
-{
-public:
-	static void PatchActivate()	 // patch out TESNPC::Activate return if the actor is reanimated
+	void Patch()
 	{
-		REL::Relocation<std::uintptr_t> Activate{ REL::ID(24211) };
+		//FastTravel
+		REL::Relocation<std::uintptr_t> target{ REL::ID(39373) };
+		stl::write_thunk_call<ClearPlayerCombatGroup>(target.address() + 0xA4C);
+	}
+}
 
-		constexpr uintptr_t START = 0x362;
-		constexpr uintptr_t END = 0x36B;
-		constexpr std::uint8_t NOP = 0x90;
+namespace Riding
+{
+	namespace Name
+	{
+		struct IsHorse
+		{
+			static bool thunk(RE::Actor*)
+			{
+				return false;
+			}
+			static inline REL::Relocation<decltype(&thunk)> func;
+		};
 
-		//.text : 0000000140360F72 cmp edx, 4
-		//.text : 0000000140360F75 jz loc_14036161D
-
-		for (uintptr_t i = START; i < END; ++i) {
-			REL::safe_write(Activate.address() + i, NOP);
+		void Patch()
+		{
+			//TESNPC::GetActivateText
+			REL::Relocation<std::uintptr_t> target{ REL::ID(24212) };
+			stl::write_thunk_call<IsHorse>(target.address() + 0x88);
 		}
 	}
 
-
-	static void PatchStolenTag()
+	namespace StolenTag
 	{
-		REL::Relocation<std::uintptr_t> ReanimateStart{ REL::ID(33956) };
+		struct CommandedEffect__Start
+		{
+			static void thunk(RE::CommandEffect* a_effect)
+			{
+				const auto zombie = a_effect->commandedActor.get();
+				const auto caster = a_effect->caster.get();
 
-		auto& trampoline = SKSE::GetTrampoline();
-		_SetupCommandedEffect = trampoline.write_call<5>(ReanimateStart.address() + 0x22B, SetupCommandedEffect);
-	}
+				if (zombie && caster && zombie != caster) {
+					auto ownerData = zombie->extraList.GetByType<RE::ExtraOwnership>();
+					if (ownerData) {
+						ownerData->owner = caster->GetActorBase();
+					}
+				}
 
-
-	static void PatchNaming()
-	{
-		REL::Relocation<std::uintptr_t> GetActivateText{ REL::ID(24212) };
-
-		auto& trampoline = SKSE::GetTrampoline();
-		_IsHorse = trampoline.write_call<5>(GetActivateText.address() + 0x88, IsHorse);
-	}
-
-private:
-	static void SetupCommandedEffect(RE::CommandEffect* a_effect)
-	{
-		auto zombiePtr = a_effect->commandedActor.get();
-		auto zombie = zombiePtr.get();
-
-		auto casterPtr = a_effect->caster.get();
-		auto caster = casterPtr.get();
-
-		if (zombie && caster && zombie != caster) {
-			auto ownerData = zombie->extraList.GetByType<RE::ExtraOwnership>();
-			if (ownerData) {
-				ownerData->owner = caster->GetActorBase();
+				func(a_effect);
 			}
+			static inline REL::Relocation<decltype(&thunk)> func;
+		};
+
+		void Patch()
+		{
+			//Reanimate::Start
+			REL::Relocation<std::uintptr_t> target{ REL::ID(33956) };
+			stl::write_thunk_call<CommandedEffect__Start>(target.address() + 0x22B);
 		}
-
-		_SetupCommandedEffect(a_effect);
 	}
-	static inline REL::Relocation<decltype(SetupCommandedEffect)> _SetupCommandedEffect;
 
-
-	static bool IsHorse(RE::Actor* a_actor)
+	namespace RaceReanimateCheck
 	{
-		return false;
-	}
-	static inline REL::Relocation<decltype(IsHorse)> _IsHorse;
-};
-
-
-//new method for patching horses, by swapping unused keyword in place of MagicNoReanimate. RemoveKeyword caused crashes (numKeywords wasn't decremented?)
-void OnInit(SKSE::MessagingInterface::Message* a_msg)
-{
-	if (a_msg->type == SKSE::MessagingInterface::kDataLoaded) {
-		if (rideHorse && patchHorse) {
-			const auto reanimateKYWD = RE::TESForm::LookupByID<RE::BGSKeyword>(0x0006F6FB);
-			const auto traitGreedy = RE::TESForm::LookupByID<RE::BGSKeyword>(0x000335FA);  //unused keyword in vanilla game.
-			if (!reanimateKYWD || !traitGreedy) {
-				return;
-			}
-
+		void Patch()
+		{
 			if (auto dataHandler = RE::TESDataHandler::GetSingleton(); dataHandler) {
+				const auto reanimateKYWD = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("MagicNoReanimate"sv);
+				if (!reanimateKYWD) {
+					return;
+				}
+
+				const auto factory = RE::IFormFactory::GetConcreteFormFactoryByType<RE::BGSKeyword>();
+				const auto dummyKYWD = factory ? factory->Create() : nullptr;
+				if (!dummyKYWD) {
+					return;
+				}
+				dummyKYWD->SetFormEditorID("DummyHorseReanimate");
+				dataHandler->GetFormArray<RE::BGSKeyword>().push_back(dummyKYWD);
+
 				for (const auto& race : dataHandler->GetFormArray<RE::TESRace>()) {
-					if (race && race->HasKeywordString("MagicNoReanimate"sv)) {
+					if (race) {
 						std::string name{ race->GetName() };
-						if (SKSE::STRING::insenstiveStringFind(name, "horse"sv)) {
-							race->SwapKeyword(reanimateKYWD, traitGreedy);
+						auto index = race->GetKeywordIndex(reanimateKYWD);
+						if (index && string::icontains(name, "horse"sv)) {
+							race->keywords[*index] = dummyKYWD;
 						}
 					}
 				}
 			}
 		}
 	}
+
+	namespace
+	{
+		void Patch()
+		{
+			//TESNPC::Activate
+			REL::Relocation<std::uintptr_t> target{ REL::ID(24211) };
+
+			constexpr uintptr_t START = 0x362;
+			constexpr uintptr_t END = 0x36B;
+
+			//.text : 0000000140360F72 cmp edx, 4
+			//.text : 0000000140360F75 jz loc_14036161D
+
+			for (uintptr_t i = START; i < END; ++i) {
+				REL::safe_write(target.address() + i, REL::NOP);
+			}
+		}
+	}
 }
 
-
-void LoadSettings()
+void OnInit(SKSE::MessagingInterface::Message* a_msg)
 {
-	constexpr auto path = L"Data/SKSE/Plugins/po3_EnhancedReanimation.ini";
-
-	CSimpleIniA ini;
-	ini.SetUnicode();
-	ini.SetMultiKey();
-
-	ini.LoadFile(path);
-
-	fastTravel = ini.GetBoolValue("Settings", "Fast Travel", true);
-	ini.SetBoolValue("Settings", "Fast Travel", fastTravel, ";fast travel fix", true);
-
-	npcCombat = ini.GetBoolValue("Settings", "Necromancer Cast", true);
-	ini.SetBoolValue("Settings", "Necromancer Cast", npcCombat, ";reanimated necromancers can cast reanimate spells", true);
-
-	decapitateCheck = ini.GetBoolValue("Settings", "Decapitated NPCs", true);
-	ini.SetBoolValue("Settings", "Decapitated NPCs", decapitateCheck, ";reanimate decapitated enemies", true);
-
-	rideHorse = ini.GetBoolValue("Riding", "Ride Horses", true);
-	ini.SetBoolValue("Riding", "Ride Horses", rideHorse, ";ride reanimated horses/mounts (not dragons)", true);
-
-	patchHorse = ini.GetBoolValue("Riding", "Patch Horses", true);
-	ini.SetBoolValue("Riding", "Patch Horses", patchHorse, ";patch all reanimate spells so mounts can be reanimated", true);
-
-	ini.SaveFile(path);
+	if (a_msg->type == SKSE::MessagingInterface::kDataLoaded) {
+		Riding::RaceReanimateCheck::Patch();
+	}
 }
 
-
-extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
-	try {
-		auto path = logger::log_directory().value() / "po3_EnhancedReanimation.log";
-		auto log = spdlog::basic_logger_mt("global log", path.string(), true);
-		log->flush_on(spdlog::level::info);
+#ifndef NDEBUG
+	auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+#else
+	auto path = logger::log_directory();
+	if (!path) {
+		return false;
+	}
+
+	*path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+#endif
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
 
 #ifndef NDEBUG
-		log->set_level(spdlog::level::debug);
-		log->sinks().push_back(std::make_shared<spdlog::sinks::msvc_sink_mt>());
+	log->set_level(spdlog::level::trace);
 #else
-		log->set_level(spdlog::level::info);
-
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::info);
 #endif
-		spdlog::set_default_logger(log);
-		spdlog::set_pattern("[%H:%M:%S] [%l] %v");
 
-		logger::info("Enhanced Reanimation {}", SOS_VERSION_VERSTRING);
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("[%H:%M:%S] %v"s);
 
-		a_info->infoVersion = SKSE::PluginInfo::kVersion;
-		a_info->name = "Enhanced Reanimation";
-		a_info->version = SOS_VERSION_MAJOR;
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
 
-		if (a_skse->IsEditor()) {
-			logger::critical("Loaded in editor, marking as incompatible");
-			return false;
-		}
+	a_info->infoVersion = SKSE::PluginInfo::kVersion;
+	a_info->name = "Enhanced Reanimation";
+	a_info->version = Version::MAJOR;
 
-		const auto ver = a_skse->RuntimeVersion();
-		if (ver < SKSE::RUNTIME_1_5_39) {
-			logger::critical("Unsupported runtime version {}", ver.string());
-			return false;
-		}
-	} catch (const std::exception& e) {
-		logger::critical(e.what());
+	if (a_skse->IsEditor()) {
+		logger::critical("Loaded in editor, marking as incompatible"sv);
 		return false;
-	} catch (...) {
-		logger::critical("caught unknown exception");
+	}
+
+	const auto ver = a_skse->RuntimeVersion();
+	if (ver < SKSE::RUNTIME_1_5_39) {
+		logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
 		return false;
 	}
 
 	return true;
 }
 
-
-extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
-	try {
-		logger::info("Enhanced Reanimation loaded");
+	logger::info("loaded plugin");
 
-		SKSE::Init(a_skse);
-		SKSE::AllocTrampoline(43);
+	SKSE::Init(a_skse);
+	SKSE::AllocTrampoline(43);
 
-		LoadSettings();
+	const auto settings = Settings::GetSingleton();
+	settings->Load();
 
-		auto messaging = SKSE::GetMessagingInterface();
-		if (!messaging->RegisterListener("SKSE", OnInit)) {
-			return false;
-		}
+	if (settings->decapitateCheck) {
+		DecapitateCheck::Patch();
+	}
+	if (settings->fastTravel) {
+		FastTravel::Patch();
+	}
+	if (settings->npcCombat) {
+		NPCCombatCast::Patch();
+	}
+	if (settings->rideHorse) {
+		Riding::Patch();
+		Riding::Name::Patch();
+		Riding::StolenTag::Patch();
 
-		if (decapitateCheck) {
-			logger::info("patching decapitate check");
-			ReanimateCheck::Patch();
+		if (settings->patchHorse) {
+			auto messaging = SKSE::GetMessagingInterface();
+			messaging->RegisterListener("SKSE", OnInit);
 		}
-		if (npcCombat) {
-			NPCCombatCast::Patch();
-			logger::info("patching NPC combat casting");
-		}
-		if (fastTravel) {
-			FastTravel::Patch();
-			logger::info("fixing fast travel bugs");
-		}
-		if (rideHorse) {
-			RideHorse::PatchActivate();
-			RideHorse::PatchStolenTag();
-			RideHorse::PatchNaming();
-			logger::info("patching zombie horse riding");
-		}
-
-	} catch (const std::exception& e) {
-		logger::critical(e.what());
-		return false;
-	} catch (...) {
-		logger::critical("caught unknown exception");
-		return false;
 	}
 
 	return true;
