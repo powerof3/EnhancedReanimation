5d804134ab11b84ec362fba1752a6bb6e6905838
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-10-13 01:25:35 +0530

    Support 1.6.353-

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,7 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
 	v.PluginVersion(Version::MAJOR);
 	v.PluginName("Enhanced Reanimation");
 	v.AuthorName("powerofthree");
-	v.UsesAddressLibrary();
-	v.UsesUpdatedStructs();
+	v.UsesAddressLibrary(true);
 	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
 
 	return v;
